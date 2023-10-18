3878fd8f8e8124fe791875c5dfe35910af0e2a33
Majestic Coding <33904170+mattmajestic@users.noreply.github.com>
2023-10-18 17:44:12 -0400

    update app

--- app.R
+++ app.R
@@ -9,7 +9,6 @@ library(DT)
 library(jsonlite)
 library(shiny)
 library(shinythemes)
-library(qrcode)
 
 wsop_url <- "https://www.wsop.com/players/profile/?playerid="
 
@@ -28,14 +27,8 @@ ui <- navbarPage(
   h4("♠️	♥ ♣  ♦"),
   br(),
   br(),
-  column(
-    4,
-    selectInput("wsop_name", "Select WSOP Player", choices = player_data$Name),
-    actionButton("show_qrcode", "Show QR Code"),
-    plotOutput("qrcode", width = "100%", height = "400px")
-  ),
-  column(8, DTOutput("wsop_dt")),
-  br()
+  selectInput("wsop_name", "Select WSOP Player", choices = player_data$Name),
+  DTOutput("wsop_dt")
 )
 
 server <- function(input, output, session) {
@@ -56,24 +49,6 @@ server <- function(input, output, session) {
         .[[1]] %>%
         html_table(fill = TRUE)
       datatable(wsop_data)
-    }
-  })
-  
-  # Render the QR code when the button is clicked
-  observeEvent(input$show_qrcode, {
-    if (!is.null(rv$id)) {
-      p <- qrcode_gen(paste0(wsop_url, rv$id), dataOutput = TRUE)
-      output$qrcode <- renderPlot({
-        heatmap(
-          p[nrow(p):1, ],
-          Rowv = NA,
-          Colv = NA,
-          scale = "none",
-          col = c("black", "white"),
-          labRow = "",
-          labCol = ""
-        )
-      })
     }
   })
   
